ee2f3ccaf34199b59fd0136d6c362253ccb975a9
Philipp Roebrock <philipp.roebrock@fhgr.ch>
2020-09-09 16:31:12 +0200

    Testbed for visualization of calibration boards

--- main.py
+++ main.py
@@ -6,15 +6,37 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import time
 
+import cv2
+import cv2.aruco as aruco
+import numpy as np
+
+aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
+squaresX = 2
+squaresY = 3
+squareLength = 10
+markerLength = 5
+board = aruco.CharucoBoard_create(squaresX, squaresY, squareLength, markerLength, aruco_dict)
+
+sidePixels = 12
+img2 = board.draw((squaresX * sidePixels, squaresY * sidePixels))
+
+img = np.zeros((img2.shape[0], img2.shape[1], 3))
+img[:,:,:] = img2[:,:,np.newaxis]
 
 mesh = MeshObject()
-mesh.load('data/pyramid.ply')
+mesh.generateFromImage(img, 5.0)
 mesh.demean()
-mesh.transform(Trafo3d(rpy=np.deg2rad([155,25,0])))
-#print(mesh)
-#mesh.show()
+mesh.transform(Trafo3d(rpy=np.deg2rad([180,0,0])))
+mesh.show(True, False, False)
 
-cam = CameraModel((100, 100), 200, T=Trafo3d(t=(0,0,-500)))
+#	mesh = MeshObject()
+#	mesh.load('data/pyramid.ply')
+#	mesh.demean()
+#	mesh.transform(Trafo3d(rpy=np.deg2rad([155,25,0])))
+#	#print(mesh)
+#	#mesh.show()
+#	
+cam = CameraModel((400, 400), 800, T=Trafo3d(t=(0,0,-500)))
 tic = time.process_time()
 dImg, cImg, P = cam.snap(mesh)
 toc = time.process_time()
