976bdd6c63d47975ea4da85657d9a12fe53fc526
Philipp Roebrock <philipp.roebrock@fhgr.ch>
2020-09-14 09:52:43 +0200

    Proper color specification of invalid pixels

--- main.py
+++ main.py
@@ -29,8 +29,9 @@ if True:
 	toc = time.process_time()
 	print(f'Snapping image took {(toc - tic):.1f}s')
 	# Color of invalid pixels
+	nan_color = (0, 0, 1.0)
 	idx = np.where(np.isnan(cImg))
-	cImg[idx[0], idx[1], :] = 0, 0, 255
+	cImg[idx[0], idx[1], :] = nan_color
 	fig = plt.figure()
 	ax = fig.add_subplot(121)
 	ax.imshow(np.transpose(cImg, axes=(1,0,2)))
@@ -38,7 +39,9 @@ if True:
 	ax.set_title('Color')
 	ax.set_aspect('equal')
 	ax = fig.add_subplot(122)
-	im = ax.imshow(dImg.T)
+	cmap = plt.cm.viridis
+	cmap.set_bad(color=nan_color, alpha=1.0)
+	im = ax.imshow(dImg.T, cmap=cmap)
 	#fig.colorbar(im, ax=ax)
 	ax.set_axis_off()
 	ax.set_title('Depth')
