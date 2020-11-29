5698183af542162a9d38454a71f7e51291d29834
Philipp Roebrock <philipp.roebrock@fhgr.ch>
2020-11-29 16:33:23 +0100

    Adds board pose generates

--- bundleadjust/generate_images.py
+++ bundleadjust/generate_images.py
@@ -15,7 +15,7 @@ np.random.seed(42) # Random but reproducible
 
 
 
-def generate_scene():
+def generate_scene(cam0_origin=True):
     # board
     squares = (6, 5)
     square_length = 50.0
@@ -42,10 +42,16 @@ def generate_scene():
     cameras.append(cam2)
     # cam 3
     cam3 = CameraModel(chip_size=(30, 30), focal_length=(30, 25))
-    cam3.place_camera((0, -300, 500))
+    cam3.place_camera((50, -300, 500))
     cam3.look_at((0, -50, 0))
-    cam3.roll_camera(np.deg2rad(-120))
+    cam3.roll_camera(np.deg2rad(-60))
     cameras.append(cam3)
+    # Put first camera coordinate system in origin
+    if cam0_origin:
+        trafo = cam0.get_camera_pose().inverse()
+        mesh_transform(board, trafo)
+        for cam in cameras:
+            cam.set_camera_pose(trafo * cam.get_camera_pose())
     return board, cameras
 
 
@@ -53,16 +59,33 @@ def generate_scene():
 def visualize_scene(board, cameras):
     cs = o3d.geometry.TriangleMesh.create_coordinate_frame(size=100.0)
     objs = [ cs, board ]
-    for cam in cameras:
-        print(cam.get_camera_pose())
+    for i, cam in enumerate(cameras):
+        print(f'cam{i}: {cam.get_camera_pose()}')
         objs.append(cam.get_cs(size=100.0))
         #objs.append(cam.get_frustum(size=100.0))
     o3d.visualization.draw_geometries(objs)
 
 
 
+def generate_board_poses(num_poses):
+    translations = np.empty((num_poses, 3))
+    translations[:,0] = np.random.uniform(-100, 100, num_poses) # X
+    translations[:,1] = np.random.uniform(-100, 100, num_poses) # Y
+    translations[:,2] = np.random.uniform(-200, 200, num_poses) # Z
+    rotations_rpy = np.empty((num_poses, 3))
+    rotations_rpy[:,0] = np.random.uniform(-10, 10, num_poses) # X
+    rotations_rpy[:,1] = np.random.uniform(-10, 10, num_poses) # Y
+    rotations_rpy[:,2] = np.random.uniform(-10, 10, num_poses) # Z
+    rotations_rpy = np.deg2rad(rotations_rpy)
+    return [ Trafo3d(t=translations[i,:],
+                     rpy=rotations_rpy[i,:]) for i in range(num_poses)]
+
+
+
 board, cameras = generate_scene()
-visualize_scene(board, cameras)
+#visualize_scene(board, cameras)
+
+board_poses = generate_board_poses(10)
 
 #depth_image, color_image, pcl = cam3.snap(board)
 #show_images(depth_image, color_image)
