8015a831e02b2ac6161b5dd9bdc27b906c4e46a6
Philipp Roebrock <philipp.roebrock@fhgr.ch>
2020-12-21 17:46:11 +0100

    Sphere position detection

--- 2d_ball_locate/run.py
+++ 2d_ball_locate/run.py
@@ -1,18 +1,55 @@
-import copy
 import cv2
 import glob
 import json
 import numpy as np
 import os
 import sys
-import open3d as o3d
-import time
 import matplotlib.pyplot as plt
-from scipy.optimize import least_squares
 
 sys.path.append(os.path.abspath('../'))
-from trafolib.trafo3d import Trafo3d
 from camsimlib.camera_model import CameraModel
+
+
+
+def detect_circle_contours(image, verbose=False):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+    thresh = cv2.threshold(blurred, 127, 255, cv2.THRESH_BINARY)[1]
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE,
+                                           cv2.CHAIN_APPROX_NONE)
+    circles = []
+    for c in contours:
+        area = cv2.contourArea(c)
+        if area > 0.8 * gray.size:
+            # contour with 80 or more percent of total image size
+            continue
+        if True:
+            # Result based on center of gravity and area->radius
+            M = cv2.moments(c)
+            circle = np.array([M["m10"] / M["m00"],
+                               M["m01"] / M["m00"],
+                               np.sqrt(area/np.pi)])
+        else:
+            # Result based on minimum enclosing circle
+            circ = cv2.minEnclosingCircle(c)
+            circle = np.array([circ[0][0], circ[0][1], circ[1]])
+        circles.append(circle)
+    if len(circles) > 0:
+        circles = np.array(circles)
+    else:
+        circles = None
+    if verbose:
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        if circles is not None:
+            for circle in circles:
+                ax.plot(*circle[0:2], 'r+')
+                circle_artist = plt.Circle(circle[0:2], circle[2],
+                                           color='r', fill=False)
+                ax.add_artist(circle_artist)
+        plt.show()
+    return circles
 
 
 
@@ -23,28 +60,37 @@ def detect_circle_hough(image, verbose=False):
     circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1, rows/8,
                                param1=40, param2=30,
                                minRadius=1, maxRadius=500)
+    if circles is not None:
+        circles = circles[0]
     if verbose:
         fig = plt.figure()
         ax = fig.add_subplot(111)
         ax.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
         if circles is not None:
-            for circle in circles[0]:
+            for circle in circles:
                 ax.plot(*circle[0:2], 'r+')
                 circle_artist = plt.Circle(circle[0:2], circle[2],
                                            color='r', fill=False)
                 ax.add_artist(circle_artist)
         plt.show()
-    if circles is not None:
-        return circles[0]
-    else:
-        return None
+    return circles
+
+
+
+def estimate_sphere_position(cam, circle_center, circle_radius, sphere_radius):
+    f = np.mean(cam.get_focal_length())
+    p = np.array([[
+            circle_center[0], circle_center[1],
+            (f * sphere_radius) / circle_radius
+            ]])
+    return cam.chip_to_scene(p)
 
 
 
 if __name__ == "__main__":
     np.random.seed(42) # Random but reproducible
     data_dir = 'a'
-    #data_dir = '/home/phil/pCloudSync/data/leafstring/bundle_adjust_trajectory'
+    #data_dir = '/home/phil/pCloudSync/data/leafstring/2d_ball_locate'
     if not os.path.exists(data_dir):
         raise Exception('Source directory does not exist.')
 
@@ -72,8 +118,24 @@ if __name__ == "__main__":
     # Run circle detection on images
     circles = np.zeros((len(images), 3))
     for i, img in enumerate(images):
-        circ = detect_circle_hough(img, False)
+        circ = detect_circle_contours(img, False)
+#        circ = detect_circle_hough(img, False)
         if circ is None or circ.shape[0] != 1:
-            circ = detect_circle_hough(img, True)
             raise Exception(f'Found more than one circle in image {i}')
         circles[i,:] = circ[0,:]
+
+    # Reconstruct sphere position from circle parameters
+    estimated_sphere_centers = np.zeros((len(images), 3))
+    for i in range(len(images)):
+        estimated_sphere_centers[i,:] = estimate_sphere_position( \
+            cam, circles[i,0:2], circles[i,2], sphere_radius)
+
+
+    errors = np.linalg.norm(estimated_sphere_centers - sphere_centers, axis=1)
+    sphere_dist = np.linalg.norm(sphere_centers, axis=1)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(sphere_dist, errors, 'o')
+    ax.grid()
+    plt.show()
