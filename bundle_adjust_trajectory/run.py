7c74d209ebaee95d2cc8d754745f302718062d43
Philipp Roebrock <philipp.roebrock@fhgr.ch>
2020-12-21 18:37:25 +0100

    Use enhanced circle detection routines in other projects as well

--- bundle_adjust_trajectory/run.py
+++ bundle_adjust_trajectory/run.py
@@ -10,7 +10,6 @@ import matplotlib.pyplot as plt
 from scipy.optimize import least_squares
 
 sys.path.append(os.path.abspath('../'))
-from trafolib.trafo3d import Trafo3d
 from camsimlib.camera_model import CameraModel
 
 
@@ -45,61 +44,68 @@ def load_files(data_dir, num_cams, num_imgs, verbose=False):
 
 
 def detect_circle_contours(image, verbose=False):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (3, 3), 0)
     thresh = cv2.threshold(blurred, 127, 255, cv2.THRESH_BINARY)[1]
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE,
                                            cv2.CHAIN_APPROX_NONE)
-    areas = []
-    centers = []
+    circles = []
     for c in contours:
         area = cv2.contourArea(c)
         if area > 0.8 * gray.size:
             # contour with 80 or more percent of total image size
             continue
-        areas.append(area)
-        M = cv2.moments(c)
-        center = np.array([M["m10"] / M["m00"], M["m01"] / M["m00"]])
-        centers.append(center)
+        if True:
+            # Result based on center of gravity and area->radius
+            M = cv2.moments(c)
+            circle = np.array([M["m10"] / M["m00"],
+                               M["m01"] / M["m00"],
+                               np.sqrt(area/np.pi)])
+        else:
+            # Result based on minimum enclosing circle
+            circ = cv2.minEnclosingCircle(c)
+            circle = np.array([circ[0][0], circ[0][1], circ[1]])
+        circles.append(circle)
+    if len(circles) > 0:
+        circles = np.array(circles)
+    else:
+        circles = None
     if verbose:
-        display_img = image.copy()
-        cv2.drawContours(display_img, contours, -1, (0, 0, 255), 3)
         fig = plt.figure()
         ax = fig.add_subplot(111)
-        ax.imshow(cv2.cvtColor(display_img, cv2.COLOR_BGR2RGB))
-        for area, center in zip(areas, centers):
-            ax.plot(center[0], center[1], 'r+')
+        ax.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        if circles is not None:
+            for circle in circles:
+                ax.plot(*circle[0:2], 'r+')
+                circle_artist = plt.Circle(circle[0:2], circle[2],
+                                           color='r', fill=False)
+                ax.add_artist(circle_artist)
         plt.show()
-    if len(centers) == 1:
-        return centers[0]
-    if verbose:
-        print(f'Error: {len(centers)} circles found.')
-    return None
+    return circles
 
 
 
 def detect_circle_hough(image, verbose=False):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (3, 3), 0)
     rows = blurred.shape[0]
     circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1, rows/8,
-                               param1=100, param2=30,
-                               minRadius=50, maxRadius=100)
-    centers = []
-    for circle in circles:
-        centers.append((circle[0][0], circle[0][1]))
+                               param1=40, param2=30,
+                               minRadius=1, maxRadius=500)
+    if circles is not None:
+        circles = circles[0]
     if verbose:
         fig = plt.figure()
         ax = fig.add_subplot(111)
-        ax.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-        for center in centers:
-            ax.plot(center[0], center[1], 'r+')
+        ax.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        if circles is not None:
+            for circle in circles:
+                ax.plot(*circle[0:2], 'r+')
+                circle_artist = plt.Circle(circle[0:2], circle[2],
+                                           color='r', fill=False)
+                ax.add_artist(circle_artist)
         plt.show()
-    if len(centers) == 1:
-        return centers[0]
-    if verbose:
-        print(f'Error: {len(centers)} circles found.')
-    return None
+    return circles
 
 
 
@@ -212,10 +218,10 @@ if __name__ == "__main__":
         cams = []
         circle_centers = []
         for cam_no, img in enumerate(images_per_cam):
-            center = detect_circle_contours(img, verbose=False)
-            if center is not None:
+            circ = detect_circle_contours(img, verbose=False)
+            if circ is not None and circ.shape[0] == 1:
                 cams.append(cameras[cam_no])
-                circle_centers.append(center)
+                circle_centers.append(circ[0,0:2])
         # Check if enough data to reconstruct
         if len(cams) < 2:
             raise Exception('At least two valid camera images needed to reconstruct trajectory step.')
@@ -249,7 +255,7 @@ if __name__ == "__main__":
     toc = time.process_time()
     print(f'Reconstructing trajectory took {(toc - tic):.1f}s')
 
-    max_error_dist = np.linalg.norm(sphere_centers - estimated_sphere_centers, axis=1)
+    max_error_dist = np.linalg.norm(estimated_sphere_centers - sphere_centers, axis=1)
     print('Real vs. estimated error distance per trajectory step')
     with np.printoptions(precision=3, suppress=True):
         print(np.vstack((max_error_dist, estimated_max_error_dist)).T)
