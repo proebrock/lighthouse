cd83180f5f571008d6c89f930ba70a9dce69e2d6
Philipp Roebrock <philipp.roebrock@fhgr.ch>
2021-07-26 20:52:01 +0200

    Adds docs on demo application

---
+++ demo/README.markdown
@@ -0,0 +1,22 @@
+# Lighthouse: Demo
+
+## Description
+
+This application shows the capabilities of the *Lighthouse* framework.
+
+First we setup the scene consisting of a red sphere embedded into a yellow plane. The camera view is slightly angled. The camera has some radial and tangential distortions.
+
+![](images/scene.png)
+
+Snapping the image lets the ray tracer intersect rays from every pixel on the camera chip with the mesh of the scene:
+
+![](images/rays.png)
+
+From the rays hitting the objects in the scene we can reconstruct color, shade and distance to the camera resulting in an RGB and depth image
+
+![](images/images.png)
+
+And of course the point cloud
+
+![](images/pcl.png)
+
--- demo/main.py
+++ demo/main.py
@@ -16,21 +16,23 @@ from camsimlib.o3d_utils import mesh_transform, mesh_generate_plane, \
 
 
 if __name__ == '__main__':
+    # Create camera and set its model parameters
     cam = CameraModel(chip_size=(40, 30),
                       focal_length=(50, 55),
                       distortion=(-0.1, 0.1, 0.05, -0.05, 0.2, 0.08))
-    cam.scale_resolution(4)
+    cam.scale_resolution(8)
     cam.place_camera((0, 0, 500))
     cam.look_at((10, 0, 0))
     cam.roll_camera(np.deg2rad(90))
     cam.move_camera_closer(50)
 
+    # Setup scene: Plane
     plane = mesh_generate_plane((200, 200), color=(1, 1, 0))
 #    plane = mesh_generate_image_file('../data/tux.png', pixel_size=3)
 #    plane = mesh_generate_charuco_board((6, 5), 30.0)
     plane.translate(-plane.get_center())
     mesh_transform(plane, Trafo3d(rpy=np.deg2rad([-25, 25, 0])))
-
+    # Setup scene: Sphere
     sphere = o3d.io.read_triangle_mesh('../data/sphere.ply')
     sphere.compute_triangle_normals()
     sphere.compute_vertex_normals()
@@ -38,19 +40,25 @@ if __name__ == '__main__':
     sphere.paint_uniform_color((1, 0, 0))
     sphere.translate((0, 0, -15))
 
-    cs = cam.get_cs(size=100.0)
-#    cs = cam.get_frustum(size=600.0)
+    # Visualize scene
+    cs = cam.get_cs(size=50.0)
+    frustum = cam.get_frustum(size=300.0)
+    o3d.visualization.draw_geometries([cs, frustum, plane, sphere])
 
-    o3d.visualization.draw_geometries([cs, plane, sphere])
-
+    # Snap image
     mesh = plane + sphere
     tic = time.monotonic()
     depth_image, color_image, pcl = cam.snap(mesh)
     toc = time.monotonic()
     print(f'Snapping image took {(toc - tic):.1f}s')
-
+    
+    # Visualize images and point cloud
     show_images(depth_image, color_image)
-#    save_shot('demo', depth_image, color_image, pcl)
+    o3d.visualization.draw_geometries([cs, pcl])
 
+    # Visualize camera rays; makes only sense with few pixels
 #    rays = mesh_generate_rays(cam.get_camera_pose().get_translation(), pcl, (0,0,0))
 #    o3d.visualization.draw_geometries([cs, plane, sphere, rays])
+
+    # Save data
+#    save_shot('demo', depth_image, color_image, pcl)
