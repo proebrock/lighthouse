b7a51c62904e1e9fcd9c7779428605634e4b1098
Philipp Roebrock <philipp.roebrock@fhgr.ch>
2021-12-30 11:02:35 +0100

    Generate simpler scene

--- stereo_vision/generate_images.py
+++ stereo_vision/generate_images.py
@@ -9,7 +9,21 @@ import time
 sys.path.append(os.path.abspath('../'))
 from camsimlib.camera_model import CameraModel
 from trafolib.trafo3d import Trafo3d
-from camsimlib.o3d_utils import mesh_transform, save_shot
+from camsimlib.o3d_utils import mesh_transform, mesh_generate_image, save_shot
+
+
+
+def generate_plane(position, color=(0, 255, 0)):
+    img = np.zeros((10, 10, 3), dtype=np.uint8)
+    img[:, :] = color
+    mesh = mesh_generate_image(img, pixel_size=10.0)
+    mesh.compute_triangle_normals()
+    mesh.compute_vertex_normals()
+    mesh.translate(-mesh.get_center()) # De-mean
+    mesh_transform(mesh, Trafo3d(rpy=np.deg2rad([0, 180, 0])))
+    mesh_pose = Trafo3d(t=position)
+    mesh_transform(mesh, mesh_pose)
+    return mesh
 
 
 
@@ -23,7 +37,7 @@ def visualize_scene(mesh, cams):
 
 
 
-def snap_and_save(cams, mesh, mesh_pose, title):
+def snap_and_save(cams, mesh, title):
     # Snap images and save
     for cidx, cam in enumerate(cams):
         basename = os.path.join(data_dir, f'{title}_cam{cidx:02d}')
@@ -36,14 +50,11 @@ def snap_and_save(cams, mesh, mesh_pose, title):
         # Save images
         # Save PCL in camera coodinate system, not in world coordinate system
         pcl.transform(cam.get_camera_pose().inverse().get_homogeneous_matrix())
-        save_shot(basename, depth_image, color_image, pcl)
+        save_shot(basename, depth_image, color_image, pcl, nan_color=(0, 0, 0))
         # Save scene properties
         params = {}
         params['cam'] = {}
         cam.dict_save(params['cam'])
-        params['world_to_object'] = {}
-        params['world_to_object']['t'] = mesh_pose.get_translation().tolist()
-        params['world_to_object']['q'] = mesh_pose.get_rotation_quaternion().tolist()
         with open(basename + '.json', 'w') as f:
             json.dump(params, f, indent=4, sort_keys=True)
 
@@ -51,26 +62,22 @@ def snap_and_save(cams, mesh, mesh_pose, title):
 
 if __name__ == "__main__":
     np.random.seed(42) # Random but reproducible
-    data_dir = 'a'
+    data_dir = 'c'
     if not os.path.exists(data_dir):
         raise Exception('Target directory does not exist.')
 
     # Generate scenes
-    mesh = o3d.io.read_triangle_mesh('../data/knot.ply')
-    mesh.compute_triangle_normals()
-    mesh.compute_vertex_normals()
-    mesh.translate(-mesh.get_center()) # De-mean
-    mesh.scale(2, center=mesh.get_center())
-    mesh_transform(mesh, Trafo3d(rpy=np.deg2rad([0, 180, 180])))
-    mesh_pose = Trafo3d(t=(0, 0, 800))
-    mesh_transform(mesh, mesh_pose)
+    mesh = generate_plane((0, 0, 500), (255, 0, 0)) # Red
+    mesh += generate_plane((100, 150, 800), (0, 255, 0)) # Green
+    mesh += generate_plane((-150, -150, 1000), (0, 0, 255)) # Blue
+    mesh += generate_plane((-150, 200, 1200), (0, 255, 255)) # Cyan
 
     # Generate cameras
     cam_left = CameraModel(chip_size=(40, 30), focal_length=(50, 50))
-    cam_left.place_camera((-120, 0, 0))
+    cam_left.place_camera((-50, 0, 0))
     cam_left.scale_resolution(30)
     cam_right = CameraModel(chip_size=(40, 30), focal_length=(50, 50))
-    cam_right.place_camera((120, 0, 0))
+    cam_right.place_camera((50, 0, 0))
     cam_right.scale_resolution(30)
     cams = [ cam_left, cam_right ]
 
@@ -85,7 +92,7 @@ if __name__ == "__main__":
 
     # Perfect setting
     #visualize_scene(mesh, cams)
-    snap_and_save(cams, mesh, mesh_pose, 'ideal')
+    snap_and_save(cams, mesh, 'ideal')
 
     # Realistic setting: Distortion
     cams[0].set_distortion((0.1, -0.1))
@@ -95,5 +102,5 @@ if __name__ == "__main__":
     T = T * Trafo3d(t=(7, 3, -14), rpy=np.deg2rad((-1.5, 3, 2)))
     cams[1].set_camera_pose(T)
     #visualize_scene(mesh, cams)
-    snap_and_save(cams, mesh, mesh_pose, 'realistic')
+    snap_and_save(cams, mesh, 'realistic')
 
