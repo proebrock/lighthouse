6c72303d643fed8a2154763abba34a69707c2470
Philipp Roebrock <philipp.roebrock@fhgr.ch>
2022-04-16 10:43:52 +0200

    Cleanup shaders

--- camsimlib/shader_parallel_light.py
+++ camsimlib/shader_parallel_light.py
@@ -18,11 +18,11 @@ class ShaderParallelLight:
 
 
 
-    def get_shadow_points(self):
+    def get_shadow_points(self, P, mesh):
         # Vector from intersection point towards light source (no point)
         lightvecs = -self.light_direction
         lightvecs = lightvecs / np.linalg.norm(lightvecs)
-        light_rt = RayTracer(self.P, lightvecs, self.mesh.vertices, self.mesh.triangles)
+        light_rt = RayTracer(P, lightvecs, mesh.vertices, mesh.triangles)
         light_rt.run()
         # When there is some part of the mesh between the intersection point camera-mesh
         # and the light source, the point lies in shade
@@ -34,16 +34,13 @@ class ShaderParallelLight:
 
     def run(self, cam, ray_tracer, mesh):
         # Extract ray tracer results
-        self.P = ray_tracer.get_points_cartesic() # shape (n, 3)
-        self.Pbary = ray_tracer.get_points_barycentric() # shape (n, 3)
-        self.triangle_idx = ray_tracer.get_triangle_indices() # shape (n, )
+        P = ray_tracer.get_points_cartesic() # shape (n, 3)
+        Pbary = ray_tracer.get_points_barycentric() # shape (n, 3)
+        triangle_idx = ray_tracer.get_triangle_indices() # shape (n, )
         # Extract vertices and vertex normals from mesh
-        self.mesh = mesh
-        self.triangles = np.asarray(self.mesh.triangles)[self.triangle_idx, :] # shape (n, 3)
-        self.vertices = np.asarray(self.mesh.vertices)[self.triangles] # shape (n, 3, 3)
-        self.vertex_normals = np.asarray(self.mesh.vertex_normals)[self.triangles] # shape (n, 3, 3)
-
-        # TODO: cleanup
+        triangles = np.asarray(mesh.triangles)[triangle_idx, :] # shape (n, 3)
+        vertices = np.asarray(mesh.vertices)[triangles] # shape (n, 3, 3)
+        vertex_normals = np.asarray(mesh.vertex_normals)[triangles] # shape (n, 3, 3)
 
         # lightvecs are unit vectors from vertex toward the light
         lightvecs = -self.light_direction
@@ -51,18 +48,19 @@ class ShaderParallelLight:
         # Dot product of vertex_normals and lightvecs; if angle between
         # those is 0°, the intensity is 1; the intensity decreases up
         # to an angle of 90° where it is 0
-        vertex_intensities = np.sum(self.vertex_normals * lightvecs, axis=2)
+        vertex_intensities = np.sum(vertex_normals * lightvecs, axis=2)
         vertex_intensities = np.clip(vertex_intensities, 0.0, 1.0)
         # From intensity determine color shade
         if mesh.has_vertex_colors():
-            vertex_colors = np.asarray(mesh.vertex_colors)[self.triangles]
+            vertex_colors = np.asarray(mesh.vertex_colors)[triangles]
         else:
-            vertex_colors = np.ones((self.triangles.shape[0], 3, 3))
+            vertex_colors = np.ones((triangles.shape[0], 3, 3))
         vertex_color_shades = vertex_colors * vertex_intensities[:, :, np.newaxis]
         # Interpolate to get color of intersection point
-        C = np.einsum('ijk, ij->ik', vertex_color_shades, self.Pbary)
-        shade_points = self.get_shadow_points()
+        C = np.einsum('ijk, ij->ik', vertex_color_shades, Pbary)
+        shade_points = self.get_shadow_points(P, mesh)
         # Points in the shade only have 10% of the originally calculated brightness
         # TODO: More physically correct model? Make this configurable?
+        # TODO: attenuation = 1.0 / (1.0 + k * distanceToLight**2) ?
         C[shade_points] *= 0.1
         return C
