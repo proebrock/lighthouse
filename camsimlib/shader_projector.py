01659de2def9e38280e8af29308cbcffdb9fb532
Philipp Roebrock <philipp.roebrock@fhgr.ch>
2022-05-14 12:55:35 +0200

    Adds first version of shader projector as well as demo program

---
+++ camsimlib/shader_projector.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+from camsimlib.projective_geometry import ProjectiveGeometry
+from camsimlib.ray_tracer_embree import RayTracer
+
+
+
+class ShaderProjector(ProjectiveGeometry):
+
+    def __init__(self, image, focal_length=100, principal_point=None,
+                 distortion=None, pose=None):
+        chip_size = (image.shape[1], image.shape[0])
+        super(ShaderProjector, self).__init__(chip_size, focal_length,
+            principal_point, distortion, pose)
+        self._image = image
+
+
+
+    def scale_resolution(self, factor=1.0):
+        super(ShaderProjector, self).scale_resolution(factor)
+        # TODO: scale self._image as well!!!???!!!
+
+
+
+    def _get_shadow_points(self, P, mesh):
+        # Vector from intersection point camera-mesh toward point light source
+        lightvecs = -P + self.get_pose().get_translation()
+        light_rt = RayTracer(P, lightvecs, mesh.vertices, mesh.triangles)
+        light_rt.run()
+        # When there is some part of the mesh between the intersection point camera-mesh
+        # and the light source, the point lies in shade
+        shade_points = light_rt.get_intersection_mask()
+        # When scale is in [0..1], the mesh is between intersection point and light source;
+        # if scale is >1, the mesh is behind the light source, so there is no intersection!
+        shade_points[shade_points] = np.logical_and( \
+            light_rt.get_scale() > 0.01, # TODO: some intersections pretty close to zero!
+            light_rt.get_scale() < 1.0)
+        return shade_points
+
+
+
+    def run(self, cam, ray_tracer, mesh):
+        # Extract ray tracer results
+        P = ray_tracer.get_points_cartesic() # shape (n, 3)
+        Pbary = ray_tracer.get_points_barycentric() # shape (n, 3)
+        triangle_idx = ray_tracer.get_triangle_indices() # shape (n, )
+        # Extract vertices and vertex normals from mesh
+        triangles = np.asarray(mesh.triangles)[triangle_idx, :] # shape (n, 3)
+        vertices = np.asarray(mesh.vertices)[triangles] # shape (n, 3, 3)
+        vertex_normals = np.asarray(mesh.vertex_normals)[triangles] # shape (n, 3, 3)
+
+        # lightvecs are unit vectors from vertex to light source
+        lightvecs = -vertices + self.get_pose().get_translation()
+        lightvecs /= np.linalg.norm(lightvecs, axis=2)[:, :, np.newaxis]
+        # Dot product of vertex_normals and lightvecs; if angle between
+        # those is 0°, the intensity is 1; the intensity decreases up
+        # to an angle of 90° where it is 0
+        vertex_intensities = np.sum(vertex_normals * lightvecs, axis=2)
+        vertex_intensities = np.clip(vertex_intensities, 0.0, 1.0)
+        # From intensity determine color shade
+        if mesh.has_vertex_colors():
+            vertex_colors = np.asarray(mesh.vertex_colors)[triangles]
+        else:
+            vertex_colors = np.ones((triangles.shape[0], 3, 3))
+        vertex_color_shades = vertex_colors * vertex_intensities[:, :, np.newaxis]
+        # Interpolate to get color of intersection point
+        C = np.einsum('ijk, ij->ik', vertex_color_shades, Pbary)
+        # In case point light source is located at camera position,
+        # there are no shade points
+        if not np.allclose(self.get_pose().get_translation(), cam.get_pose().get_translation()):
+            shadow_points = self._get_shadow_points(P, mesh)
+            # Points in the shade only have 10% of the originally calculated brightness
+            # TODO: More physically correct model? Make this configurable?
+            # TODO: attenuation = 1.0 / (1.0 + k * distanceToLight**2) ?
+            C[shadow_points] *= 0.1
+        return C
---
+++ demo/projector/run.py
@@ -0,0 +1,73 @@
+import os
+import sys
+import time
+
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import open3d as o3d
+
+sys.path.append(os.path.abspath('../../'))
+from trafolib.trafo3d import Trafo3d
+from camsimlib.camera_model import CameraModel
+from camsimlib.shader_projector import ShaderProjector
+from camsimlib.o3d_utils import show_images
+
+
+
+if __name__ == '__main__':
+    # Camera
+    cam = CameraModel(chip_size=(120, 90),
+                      focal_length=(100, 100),
+                    )
+    #cam.set_distortion((-0.1, 0.1, 0.05, -0.05, 0.2, 0.08))
+    cam.scale_resolution(1)
+    cam.place((-500, 0, 500))
+    cam.look_at((0, 0, 0))
+    cam.roll(np.deg2rad(-90))
+
+    # Image file displayed by projector
+    projector_image_filename = '../../data/tux.png'
+    projector_image = cv2.imread(projector_image_filename, cv2.IMREAD_COLOR)
+    if projector_image is None:
+        raise Exception(f'Unable to read image file "{projector_image_filename}"')
+    projector_image = cv2.cvtColor(projector_image, cv2.COLOR_BGR2RGB)
+    projector_image = projector_image.astype(float) / 255
+    if False:
+        fig, ax = plt.subplots()
+        ax.imshow(projector_image)
+        plt.show()
+
+    # Shader implementing projector
+    projector = ShaderProjector(image=projector_image, focal_length=(100, 100))
+    projector.scale_resolution(1)
+    projector.place((-600, 0, 200))
+    projector.look_at((0, 0, 0))
+    projector.roll(np.deg2rad(-90))
+
+    # Object
+    mesh = o3d.io.read_triangle_mesh('../../data/fox_head.ply')
+    mesh.compute_triangle_normals()
+    mesh.compute_vertex_normals()
+    mesh.translate(-mesh.get_center())
+    mesh.scale(200, center=(0, 0, 0))
+
+    # Visualize scene
+    if False:
+        world_cs = o3d.geometry.TriangleMesh.create_coordinate_frame(size=200.0)
+        proj_cs = projector.get_cs(size=50.0)
+        proj_frustum = projector.get_frustum(size=100.0)
+        cam_cs = cam.get_cs(size=50.0)
+        cam_frustum = cam.get_frustum(size=100.0)
+        o3d.visualization.draw_geometries([world_cs, mesh, \
+            cam_cs, cam_frustum, proj_cs, proj_frustum])
+
+    # Snap image
+    tic = time.monotonic()
+    depth_image, color_image, pcl = cam.snap(mesh, shaders=[projector])
+    toc = time.monotonic()
+    print(f'Snapping image took {(toc - tic):.1f}s')
+
+    # Visualize images and point cloud
+    show_images(depth_image, color_image)
+    #o3d.visualization.draw_geometries([cs, pcl])
