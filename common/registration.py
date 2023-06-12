1a025341a5cbba58dee84b719ecf44e41d62cbee
Philipp Roebrock <philipp.roebrock@fhgr.ch>
2023-06-12 15:59:27 +0200

    Adds scale estimation and tests to registration

--- common/registration.py
+++ common/registration.py
@@ -28,7 +28,7 @@ def orthogonal_procrustes(p0, p1):
 
 
 
-def estimate_transform(p0, p1):
+def estimate_transform(p0, p1, estimate_scale=False):
     """ Estimate trafo between two point clouds
     Expects correspondences between both point clouds:
     Point p0[i,:] must correspond to p1[:,i] for all i in [0..num_points].
@@ -37,16 +37,21 @@ def estimate_transform(p0, p1):
     :return: Trafo3d object of estimated transformation from p0 to p1
     """
     assert p0.shape == p1.shape
-    assert p0.shape[1] == 3 # Just tested for 3D
-    # De-mean point clouds
+    # Estimate translations and de-mean
     m0 = np.mean(p0, axis=0)
     m1 = np.mean(p1, axis=0)
     pp0 = p0 - m0
     pp1 = p1 - m1
     # Estimate rotation matrix
     R = orthogonal_procrustes(pp0, pp1)
-    # Determine translation
-    t = m0 - np.dot(R, m1)
-    return Trafo3d(t=t, mat=R)
+    # Estimate scale
+    if estimate_scale:
+        s0 = np.linalg.norm(pp0)
+        s1 = np.linalg.norm(pp1)
+        scale = s0 / s1
+        t = m0 - np.dot(R, scale * m1)
+        return Trafo3d(t=t, mat=R), scale
+    else:
+        t = m0 - np.dot(R, m1)
+        return Trafo3d(t=t, mat=R)
 
-# TODO: https://github.com/theochem/procrustes
---
+++ common/test_registration.py
@@ -0,0 +1,47 @@
+import numpy as np
+np.random.seed(42)
+import pytest
+
+from trafolib.trafo3d import Trafo3d
+from common.registration import estimate_transform
+
+
+
+def test_estimate_transform_3d_without_scale():
+    # Generate points cloud
+    n = 100
+    p1 = np.random.uniform(-10.0, 10.0, (n, 3))
+    # Set nominal transformation parameters
+    p0_to_p1 = Trafo3d(t=(10, 20, -33), rpy=np.deg2rad((-70, 120, 5)))
+    # Calculate other point cloud
+    p0 = p0_to_p1 * p1
+    # Estimate transformation
+    p0_to_p1_estim = estimate_transform(p0, p1)
+    # Check results
+    dt, dr = p0_to_p1.distance(p0_to_p1_estim)
+    assert np.isclose(dt, 0.0)
+    assert np.isclose(dr, 0.0)
+
+
+
+def test_estimate_transform_3d_with_scale():
+    # Generate points cloud
+    n = 100
+    p1 = np.random.uniform(-100.0, 100.0, (n, 3))
+    # Set nominal transformation parameters
+    p0_to_p1 = Trafo3d(t=(-111, 222, -303), rpy=np.deg2rad((30, -70, -145)))
+    scale = 13.0
+    # Calculate other point cloud
+    # (with three homogenous matrices for translation (T), rotation (R) and
+    # scaling (S), the total transformation would look like
+    # p0 = T * R * S * p1; T * R is done in Trafo3d, so scale has to be
+    # multiplied first
+    p0 = p0_to_p1 * (scale * p1)
+    # Estimate transformation
+    p0_to_p1_estim, scale_estim = estimate_transform(p0, p1, estimate_scale=True)
+    # Check results
+    dt, dr = p0_to_p1.distance(p0_to_p1_estim)
+    assert np.isclose(dt, 0.0)
+    assert np.isclose(dr, 0.0)
+    assert np.isclose(scale, scale_estim)
+
