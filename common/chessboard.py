fb523508730008ac9f7a32b31fdafd6317f049c7
Philipp Roebrock <philipp.roebrock@fhgr.ch>
2023-08-14 13:27:20 +0200

    Chessboard corner detection added

--- common/chessboard.py
+++ common/chessboard.py
@@ -2,19 +2,27 @@ import sys
 import os
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+import open3d as o3d
 
 
 sys.path.append(os.path.abspath('../'))
 from trafolib.trafo3d import Trafo3d
 from common.image_utils import image_show
+from camsimlib.screen import Screen
+from camsimlib.camera_model import CameraModel
 
 
 
 class Chessboard:
 
-    def __init__(self, squares=(5, 7), square_length_pix=80, square_length_mm=20.0,
-        pose=Trafo3d()):
+    def __init__(self, squares=(5, 6), square_length_pix=80,
+        square_length_mm=20.0, pose=Trafo3d()):
         self._squares = np.asarray(squares)
+        assert self._squares.size == 2
+        # One dimension has to be odd, the other even to
+        # have a rotation invariant chessboard
+        assert np.sum(self._squares % 2) == 1
         self._square_length_pix = square_length_pix
         self._square_length_mm = square_length_mm
         self._pose = pose
@@ -28,6 +36,67 @@ class Chessboard:
         param_dict = {}
         self.dict_save(param_dict)
         return str(param_dict)
+
+
+
+    def get_pose(self):
+        """ Get transformation from world to center of MultiAruco object
+        :return: Pose as Trafo3d object
+        """
+        return self._pose
+
+
+
+    def set_pose(self, pose):
+        """ Set transformation from world to center of MultiAruco object
+        :param pose: Pose as Trafo3d object
+        """
+        self._pose = pose
+
+
+
+    def get_cs(self, size):
+        """ Get coordinate system object representing pose of MultiMarker object
+        :param size: Length of coordinate axes
+        :return: Coordinate system as Open3D mesh object
+        """
+        cs = o3d.geometry.TriangleMesh.create_coordinate_frame(size=size)
+        cs.transform(self._pose.get_homogeneous_matrix())
+        return cs
+
+
+
+    def get_size_pix(self):
+        """ Get size of board in pixels
+        :return: Size as tupel in (X, Y)
+        """
+        return self._squares * self._square_length_pix
+
+
+
+    def get_size_mm(self):
+        """ Get size of board in millimeters
+        :return: Size as tupel in (X, Y)
+        """
+        return self._squares * self._square_length_mm
+
+
+
+    def get_pixelsize_mm(self):
+        """ Get size of a single pixel in millimeters
+        :return: Size as a scalar
+        """
+        return self._square_length_mm / self._square_length_pix
+
+
+
+    def get_resolution_dpi(self):
+        """ Get resolution of board in DPI (dots per inch)
+        Use this resolution to print the board on paper to get correct dimensions.
+        :return: Resolution
+        """
+        mm_per_inch = 25.4
+        return (self._square_length_pix * mm_per_inch) / self._square_length_mm
 
 
 
@@ -55,23 +124,16 @@ class Chessboard:
 
 
 
-    def get_resolution_dpi(self):
-        """ Get resolution of board in DPI (dots per inch)
-        Use this resolution to print the board on paper to get correct dimensions.
-        :return: Resolution
-        """
-        mm_per_inch = 25.4
-        return (self._square_length_pix * mm_per_inch) / self._square_length_mm
-
-
-
     def generate_image(self):
         """ Generates a 2D bitmap RGB image of the board
         :return: Image, shape (height, width, 3)
         """
-        width = self._squares[0] * self._square_length_pix
-        height = self._squares[1] * self._square_length_pix
-        image = np.zeros((width, height), dtype=np.uint8)
+        l = self._square_length_pix
+        image = np.zeros(self._squares * l, dtype=np.uint8)
+        for row in range(self._squares[0]):
+            col_start = 0 if (row % 2) == 0 else 1
+            for col in range(col_start, self._squares[1], 2):
+                image[row*l:(row+1)*l,col*l:(col+1)*l] = 255
         return cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
 
 
@@ -82,3 +144,91 @@ class Chessboard:
         image = self.generate_image()
         title = f'squares {self._squares}, shape {image.shape}, dpi {self.get_resolution_dpi():.0f}'
         image_show(image, title)
+
+
+
+    def plot3d(self):
+        """ Shows 3D image of board with a coordinate system
+        """
+        cs_size = np.min(self._squares) * self._square_length_mm
+        cs = self.get_cs(cs_size)
+        mesh = self.generate_mesh()
+        o3d.visualization.draw_geometries([cs, mesh])
+
+
+
+    def generate_screen(self):
+        """ Generate a screen object with dimensions of board and its image
+        :return: Screen object
+        """
+        image = self.generate_image()
+        return Screen(self.get_size_mm(), image, self._pose)
+
+
+
+    def detect_obj_img_points(self, image):
+        """ Detects object and image points in image
+        :param image: RBG image of shape (height, width, 3)
+        :param verbose: Show plot of detected corners and IDs
+        :return: Lists of object points and image points
+        """
+        assert isinstance(image, np.ndarray)
+        assert image.ndim == 3
+        assert image.shape[2] == 3 # RGB image
+        assert image.dtype == np.uint8 # 8-bit
+        success, corners = cv2.findChessboardCorners(image, self._squares - 1)
+        corners = np.array(corners).reshape((-1, 2))
+        if not success or corners.shape[0] != np.prod(self._squares - 1):
+            raise Exception('Unable to detect all checkboard coners.')
+        if True:
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            ax.imshow(image)
+            for i in range(corners.shape[0]):
+                ax.plot(corners[i, 0], corners[i, 1], '+r')
+                ax.text(corners[i, 0], corners[i, 1], f'{i}', color='r')
+            plt.show()
+
+        sdaf
+
+
+
+    def detect_all_obj_img_points(self, images):
+        """ Convenience function to detect all image points in a stack of images
+        :param images: Stack of n images, shape (n, height, width, 3)
+        :return list of object_points, list of image_points
+        """
+        obj_points = []
+        img_points = []
+        for image in images:
+            _, op, ip = self.detect_obj_img_points(image)
+            #MultiAruco._plot_correspondences(op, ip, image)
+            obj_points.append(op)
+            img_points.append(ip)
+        return obj_points, img_points
+
+
+
+    def calibrate_intrinsics(self, images, flags=0):
+        """ Calibrates intrinsics of a camera from a stack of images
+        :param images: Stack of n images, shape (n, height, width, 3)
+        :param flags: Calibration flags from OpenCV
+        :return: Camera model, trafos from camera to each board, reprojection error
+        """
+        # Extract object and image points
+        obj_points, img_points = self.detect_all_obj_img_points(images)
+        # Calibrate camera
+        image_shape = images.shape[1:3]
+        reprojection_error, camera_matrix, dist_coeffs, rvecs, tvecs = \
+            cv2.calibrateCamera(obj_points, img_points, \
+            image_shape, None, None, flags=flags)
+        # Generate camera object and set intrincis
+        cam = CameraModel()
+        cam.set_chip_size((images.shape[2], images.shape[1]))
+        cam.set_camera_matrix(camera_matrix)
+        cam.set_distortion(dist_coeffs)
+        # Set extrinsics
+        cam_to_boards = []
+        for rvec, tvec in zip(rvecs, tvecs):
+            cam_to_boards.append(Trafo3d(rodr=rvec, t=tvec))
+        return cam, cam_to_boards, reprojection_error
--- common/test_chessboard.py
+++ common/test_chessboard.py
@@ -1,18 +1,75 @@
 import sys
 import os
 import pytest
+import numpy as np
 import matplotlib.pyplot as plt
+import open3d as o3d
+import cv2
 
 
 
 sys.path.append(os.path.abspath('../'))
 from common.chessboard import Chessboard
 from trafolib.trafo3d import Trafo3d
+from common.image_utils import image_3float_to_rgb, image_show_multiple
+from camsimlib.camera_model import CameraModel
 
 
 
-def test_calibrate_intrinsics():
-    board = Chessboard()
-    board.plot2d()
-    plt.show()
+def generate_board_poses(random_generator, num_poses):
+    translations = np.empty((num_poses, 3))
+    translations[:,0] = random_generator.uniform(-100, 100, num_poses) # X
+    translations[:,1] = random_generator.uniform(-100, 100, num_poses) # Y
+    translations[:,2] = random_generator.uniform(-200, 200, num_poses) # Z
+    rotations_rpy = np.empty((num_poses, 3))
+    rotations_rpy[:,0] = random_generator.uniform(-20, 20, num_poses) # X
+    rotations_rpy[:,1] = random_generator.uniform(-20, 20, num_poses) # Y
+    rotations_rpy[:,2] = random_generator.uniform(-20, 20, num_poses) # Z
+    rotations_rpy = np.deg2rad(rotations_rpy)
+    return [ Trafo3d(t=translations[i,:],
+                     rpy=rotations_rpy[i,:]) for i in range(num_poses)]
+
+
+
+def test_calibrate_intrinsics(random_generator):
+    """ Test: single uncalibrated camera, single CharucoBoard, multiple images;
+    calibration of intrinsics of camera
+    """
+    # Prepare scene: CharucoBoard and Screen
+    board = Chessboard(squares=(5, 6), square_length_pix=80,
+        square_length_mm=20.0)
+    screen = board.generate_screen()
+    # Prepare scene: CameraModel: Looks orthogonally in the middle of board
+    cam = CameraModel(chip_size=(40, 30), focal_length=(50, 50))
+    half_board_size = board.get_size_mm() / 2.0
+    cam.place((half_board_size[0], half_board_size[1], -700))
+    cam.look_at((half_board_size[0], half_board_size[1], 0))
+    cam.roll(np.deg2rad(90))
+    cam.scale_resolution(30)
+    # Visualize
+    if False:
+        screen_cs = screen.get_cs(size=100)
+        screen_mesh = screen.get_mesh()
+        cam_cs = cam.get_cs(size=100)
+        cam_frustum = cam.get_frustum(size=200)
+        o3d.visualization.draw_geometries([screen_cs, screen_mesh, cam_cs, cam_frustum])
+    # Snap images
+    num_images = 12
+    world_to_screens = generate_board_poses(random_generator, num_images)
+    chip_size = cam.get_chip_size()
+    images = np.zeros((num_images, chip_size[1], chip_size[0], 3), dtype=np.uint8)
+    for i in range(num_images):
+        print(f'Snapping image {i+1}/{num_images} ...')
+        screen.set_pose(world_to_screens[i])
+        screen_mesh = screen.get_mesh()
+        _, image, _ = cam.snap(screen_mesh)
+        images[i, :, :, :] = image_3float_to_rgb(image)
+    if False:
+        image_show_multiple(images, single_window=True)
+        plt.show()
+    # Identify simple camera model
+    flags = cv2.CALIB_ZERO_TANGENT_DIST | \
+        cv2.CALIB_FIX_K1 | cv2.CALIB_FIX_K2 | cv2.CALIB_FIX_K3
+    cam_recalib, cam_to_boards_estim, reprojection_error = \
+        board.calibrate_intrinsics(images, flags=flags)
 
