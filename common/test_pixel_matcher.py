9fc16210899c060c7093c900377cba5827ab0ef8
Philipp Roebrock <philipp.roebrock@fhgr.ch>
2023-12-25 14:48:21 +0100

    Moves downscaling test from <image> to <line> level

--- common/test_pixel_matcher.py
+++ common/test_pixel_matcher.py
@@ -43,6 +43,26 @@ def test_line_matcher_roundtrip(LineMatcherImplementation):
 
 
 
+def test_line_matcher_upscale(LineMatcherImplementation):
+    # Generate low resolution lines
+    n = 400
+    matcher = LineMatcherImplementation(n)
+    lines_low = matcher.generate()
+    # Upscale lines by duplicating each pixel
+    lines_high = np.zeros((matcher.num_time_steps(), 2*n), dtype=np.uint8)
+    lines_high[:, 0::2] = lines_low
+    lines_high[:, 1::2] = lines_low
+    indices = matcher.match(lines_high)
+    # Check matches
+    expected_indices = np.zeros(2*n)
+    expected_indices[0::2] = np.arange(n)
+    expected_indices[1::2] = np.arange(n)
+    # Compare with matches
+    diff = np.round(indices).astype(int) - expected_indices
+    assert np.all(diff == 0)
+
+
+
 def generate_image_roundtrip_indices(shape):
     expected_indices = np.zeros((shape[0], shape[1], 2), dtype=int)
     i0 = np.arange(shape[0])
@@ -75,50 +95,6 @@ def test_image_matcher_roundtrip(LineMatcherImplementation):
     # Check results
     expected_indices = generate_image_roundtrip_indices(shape)
     diff = np.round(indices).astype(int) - expected_indices
-    assert np.all(diff == 0)
-
-
-
-def image_double_upscale(image):
-    """ Up-scales a 2D image to double the resolution duplicating every four times
-    """
-    assert image.ndim == 2
-    result = np.zeros((2 * image.shape[0], 2 * image.shape[1]), dtype=image.dtype)
-    result[0::2, 0::2] = image
-    result[0::2, 1::2] = image
-    result[1::2, 0::2] = image
-    result[1::2, 1::2] = image
-    return result
-
-
-
-def image_stack_double_upscale(images):
-    assert images.ndim == 3
-    result = np.zeros((images.shape[0], 2 * images.shape[1], 2* images.shape[2]),
-        dtype=images.dtype)
-    for i in range(images.shape[0]):
-        result[i, :, :] = image_double_upscale(images[i, :, :])
-    return result
-
-
-
-def test_image_matcher_roundtrip_double_resolution(LineMatcherImplementation):
-    # Generate images
-    shape = (60, 80)
-    row_matcher = LineMatcherImplementation(shape[0])
-    col_matcher = LineMatcherImplementation(shape[1])
-    matcher = ImageMatcher(shape, row_matcher, col_matcher)
-    images = matcher.generate()
-    images = image_stack_double_upscale(images)
-    # Match
-    indices = matcher.match(images)
-    # Check results
-    expected_indices = generate_image_roundtrip_indices(shape)
-    diff = np.round(indices[:, :, 0]).astype(int) - \
-        image_double_upscale(expected_indices[:, :, 0])
-    assert np.all(diff == 0)
-    diff = np.round(indices[:, :, 1]).astype(int) - \
-        image_double_upscale(expected_indices[:, :, 1])
     assert np.all(diff == 0)
 
 
