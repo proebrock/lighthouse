378b0edd1a8bc145efd87c96bb80bcd855f5de17
Philipp Roebrock <philipp.roebrock@fhgr.ch>
2023-12-30 10:10:30 +0100

    Adds new unfinished test

--- camsimlib/test_shaders.py
+++ camsimlib/test_shaders.py
@@ -9,6 +9,7 @@ from common.mesh_utils import mesh_generate_plane
 from . camera_model import CameraModel
 from . shader_parallel_light import ShaderParallelLight
 from . shader_point_light import ShaderPointLight
+from . shader_projector import ShaderProjector
 
 
 
@@ -107,4 +108,51 @@ def test_shader_color():
         assert np.all(np.isclose(color_image[:, :, 0], test_color[0])) # R
         assert np.all(np.isclose(color_image[:, :, 1], test_color[1])) # G
         assert np.all(np.isclose(color_image[:, :, 2], test_color[2])) # B
+
+
+
+@pytest.mark.skip(reason="work in progess")
+def test_projector_shader_color_blending():
+    """ Put plane of different colors in front of a camera, take pictures
+    using a projector shader with solid color and check if color taken by
+    camera have the expected color
+    """
+    # Cam
+    cam = CameraModel(chip_size=(60, 45),
+                      focal_length=(120, 90),
+                    )
+    cam_pose = Trafo3d(t=(0, 0, 300), rpy=np.deg2rad((180, 0, 0)))
+    cam.set_pose(cam_pose)
+    # Object: Plane that covers the full view of the camera
+    plane = mesh_generate_plane((400, 400), color=(0.5, 0, 0))
+    plane.translate(-plane.get_center())
+    # Projector
+    image = np.zeros((45, 60, 3), dtype=np.uint8)
+    projector = ShaderProjector(image, focal_length=(120, 90), pose=cam_pose)
+    # Visualize scene
+    if True:
+        world_cs = o3d.geometry.TriangleMesh.create_coordinate_frame(size=100.0)
+        cam_cs = cam.get_cs(size=50.0)
+        cam_frustum = cam.get_frustum(size=100.0)
+        projector_cs = projector.get_cs(size=30.0)
+        projector_frustum = projector.get_frustum(size=70.0)
+        o3d.visualization.draw_geometries([world_cs, plane, cam_cs, cam_frustum,
+            projector_cs, projector_frustum])
+    # Define tests
+    test_colors = [
+        # object color   light color      expected resulting color
+        [ [1, 1, 1],     [ 1, 1, 1 ],    [ 0.5, 0, 0 ] ],
+    ]
+    # Run tests
+    for ocolor, lcolor, ecolor in test_colors:
+        plane.paint_uniform_color(ocolor)
+        image = np.tile(np.array(lcolor, dtype=np.uint8), (45, 60, 1))
+        image = np.ones((45, 60, 3), dtype=np.uint8)
+        projector.set_image(image)
+        _, color_image, _ = cam.snap(plane, [ projector ])
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.imshow(color_image)
+        plt.show()
 
